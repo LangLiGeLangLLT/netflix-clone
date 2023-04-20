82bb236ca25b31e31a88a53ebc882db6eab00c41
llt <418797644@qq.com>
2023-04-20 21:53:27 +0800

    feat: update

--- components/AccountMenu.tsx
+++ components/AccountMenu.tsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import { signOut } from 'next-auth/react'
+import useCurrentUser from '@/hooks/useCurrentUser'
 
 interface AccountMenuProps {
   visible?: boolean
 }
 
 export default function AccountMenu({ visible }: AccountMenuProps) {
+  const { data } = useCurrentUser()
+
   if (!visible) {
     return null
   }
+
   return (
     <div className="bg-black w-56 absolute top-14 right-0 py-5 flex-col border-2 border-gray-800 flex">
       <div className="flex flex-col gap-3">
@@ -19,12 +23,12 @@ export default function AccountMenu({ visible }: AccountMenuProps) {
             alt=""
           />
           <p className="text-white text-sm group-hover/item:underline">
-            Username
+            {data?.name}
           </p>
         </div>
         <hr className="bg-gray-600 border-0 h-px my-4" />
         <div
-          onClick={() => {}}
+          onClick={() => signOut()}
           className="px-3 text-center text-white text-sm hover:underline"
         >
           Sign out of Netflix
--- components/Billboard.tsx
+++ components/Billboard.tsx
@@ -1,11 +1,17 @@
-import useBillboard from '@/hooks/useBillBoard'
-import React from 'react'
+import useBillboard from '@/hooks/useBillboard'
+import React, { useCallback } from 'react'
 
 import { AiOutlineInfoCircle } from 'react-icons/ai'
 import PlayButton from './PlayButton'
+import useInfoModal from '@/hooks/useInfoModal'
 
 export default function Billboard() {
   const { data } = useBillboard()
+  const { openModal } = useInfoModal()
+
+  const handleOpenModal = useCallback(() => {
+    openModal(data?.id)
+  }, [openModal, data?.id])
 
   return (
     <div className="relative h-[56.25vw]">
@@ -24,7 +30,10 @@ export default function Billboard() {
         <p className="text-white text-[8px] md:text-lg mt-3 md:,mt-8 w-[80%] md:w-[80%] lg:w-[50%] drop-shadow-xl">
           {data?.description}
         </p>
-        <div className="flex flex-row items-center mt-3 md:mt-4 gap-3">
+        <div
+          onClick={handleOpenModal}
+          className="flex flex-row items-center mt-3 md:mt-4 gap-3"
+        >
           <PlayButton movieId={data?.id} />
           <button className="bg-white text-white bg-opacity-30 rounded-md py-1 md:py-2 px-2 md:px-4 w-auto text-xl lg:text-lg font-semibold flex flex-row items-center hover:bg-opacity-20 transition">
             <AiOutlineInfoCircle className="mr-1" />
